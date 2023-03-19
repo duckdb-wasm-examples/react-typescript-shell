8c11fd79a2974258f5504ecf51819732ab95c525
pgzmnk <pgzmnk@gmail.com>
2023-03-19 15:47:49 -0600

    ignore .tsx import lint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './static/index.css';
-import { App } from './App';
+// @ts-ignore
+import { App } from './App.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
