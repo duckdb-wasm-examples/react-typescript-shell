c8f6feb1f1799c0266e22d7a11baff1ea5d1571c
pgzmnk <pgzmnk@gmail.com>
2023-03-19 15:34:32 -0600

    Imports.

--- src/app.tsx
+++ src/app.tsx
@@ -2,19 +2,11 @@ import React from 'react';
 import logo from './static/logo.svg';
 import './static/App.css';
 import 'xterm/css/xterm.css';
-import styles from './static/app.module.css';
-
-import * as duckdb from '@duckdb/duckdb-wasm';
-import duckdb_wasm from '@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm';
-import duckdb_wasm_eh from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm';
-import * as shell from '@duckdb/duckdb-wasm-shell';
-import shell_wasm from '@duckdb/duckdb-wasm-shell/dist/shell_bg.wasm';
-
 import { Shell } from './components/Shell';
 
 type SomeComponentProps = Record<string, string>;
 
-const App: React.FC<SomeComponentProps> = (propr: SomeComponentProps) => {
+export const App: React.FC<SomeComponentProps> = (propr: SomeComponentProps) => {
     const [count, setCount] = React.useState<number>(0);
 
     React.useEffect(() => {
@@ -33,4 +25,3 @@ const App: React.FC<SomeComponentProps> = (propr: SomeComponentProps) => {
     );
 };
 
-export default App;
