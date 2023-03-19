64480f0788e41ff6da37659bea71d6abb64f47f7
Plinio Guzman <pgzmnk@gmail.com>
2023-03-19 15:56:28 -0600

    Casing

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './static/logo.svg';
+import './static/App.css';
+import 'xterm/css/xterm.css';
+import { Shell } from './components/Shell';
+
+type SomeComponentProps = Record<string, string>;
+
+export const App: React.FC<SomeComponentProps> = (propr: SomeComponentProps) => {
+    const [count, setCount] = React.useState<number>(0);
+
+    React.useEffect(() => {
+        document.title = `You clicked ${count} times`;
+    });
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Click count: {count}</p>
+                <button onClick={() => setCount(count + 1)}>Click me</button>
+            </header>
+            <Shell />
+        </div>
+    );
+};
